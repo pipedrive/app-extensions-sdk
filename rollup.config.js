34c7a2df38808f320a3a1e3e91ec881d793d56f9
Andreas Saltsberg <andreassaltsberg@Andreass-MacBook-Pro.local>
2022-07-14 14:42:39 +0300

    Add UMD entrypoint

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: 'dist/index.js',
-			format: 'cjs',
-			sourcemap: true,
-			exports: 'named',
-		},
-		{
-			file: 'dist/index.umd.js',
-			format: 'umd',
-			sourcemap: true,
-			exports: 'named',
-			name: 'AppExtensionsSdk',
-		},
-	],
-	plugins: [typescript({ tsconfig: './tsconfig.json' })],
-};
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: 'dist/index.js',
+				format: 'cjs',
+				sourcemap: true,
+				exports: 'named',
+			},
+		],
+		plugins: [typescript({ tsconfig: './tsconfig.json' })],
+	},
+	{
+		input: 'src/umd.ts',
+		output: [
+			{
+				file: 'dist/index.umd.js',
+				format: 'umd',
+				sourcemap: true,
+				exports: 'default',
+				name: 'AppExtensionsSdk',
+			},
+		],
+		plugins: [typescript({ tsconfig: './tsconfig.json' })],
+	},
+]
---
+++ src/umd.ts
@@ -0,0 +1,11 @@
+import AppExtensionsSDK, { Command, Event, Modal, ModalStatus, Color, TrackingEvent, MessageType } from './index';
+
+export default Object.assign(AppExtensionsSDK, {
+	Command,
+	Event,
+	Modal,
+	ModalStatus,
+	Color,
+	TrackingEvent,
+	MessageType,
+});
