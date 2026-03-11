e6b5e32044caca09ff74f657fb177c9c65458efe
Edmilson De Souza <edmilson.souza@pipedrive.com>
2026-03-11 14:35:38 +0200

    Revert vulnerability fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,5 @@
-import { webcrypto } from 'node:crypto';
 import typescript from '@rollup/plugin-typescript';
-
-// serialize-javascript (used by @rollup/plugin-terser) uses the bare `crypto`
-// global which is only available on Node 19+. Polyfill for Node 16-18.
-// Must run before @rollup/plugin-terser is imported (ESM static imports are
-// hoisted, so terser is loaded dynamically after the polyfill is applied).
-if (typeof crypto === 'undefined') {
-	globalThis.crypto = webcrypto;
-}
-
-const { default: terser } = await import('@rollup/plugin-terser');
+import { terser } from 'rollup-plugin-terser';
 
 export default [
 	{
